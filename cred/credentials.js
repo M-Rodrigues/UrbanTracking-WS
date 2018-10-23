269589a09abd28de58b0d60b71660bb507b35664
Matheus Rodrigues <matheus.rodrigues.ime@gmail.com>
2018-10-23 10:46:05 -0300

    cred

---
+++ cred/credentials.js
@@ -0,0 +1,11 @@
+module.exports = {
+    DATABASE_URL: "postgres://ainswidasefaqi:b9e3a0fb257502f315e38f7446b7537cc19b60a2ba3f576fcdffac2fc9bc8c42@ec2-54-243-61-194.compute-1.amazonaws.com:5432/dr5guq579ouq1",
+    DATABASE_CONN_CONFIG: {
+        user: 'ainswidasefaqi',
+        host: 'ec2-54-243-61-194.compute-1.amazonaws.com',
+        database: 'dr5guq579ouq1',
+        password: 'b9e3a0fb257502f315e38f7446b7537cc19b60a2ba3f576fcdffac2fc9bc8c42',
+        port: 5432,
+        ssl: true
+      }
+}
