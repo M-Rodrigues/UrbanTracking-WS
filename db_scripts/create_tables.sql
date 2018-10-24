615d4ee38615fa190c88c3d6ab1eebd1a1287d2e
Matheus Rodrigues <matheus.rodrigues.ime@gmail.com>
2018-10-24 13:18:09 -0300

    tabelas para monitoramento das composicoes

--- db_scripts/create_tables.sql
+++ db_scripts/create_tables.sql
@@ -49,3 +49,42 @@ CREATE TABLE trajeto
         REFERENCES rota (id)
 )
 
+CREATE TABLE composicao
+(
+    id SERIAL,
+    cod_rastreador INTEGER NOT NULL,
+    lat FLOAT NOT NULL,
+    lng FLOAT NOT NULL,
+    ultima_atualizacao TIMESTAMP NOT NULL,
+    idmodal BIGINT NOT NULL,
+    CONSTRAINT pk_composicao PRIMARY KEY (id),
+    CONSTRAINT fk_modal FOREIGN KEY (idmodal)
+        REFERENCES modal (id)
+)
+
+CREATE TABLE corrida
+(
+    id SERIAL,
+    idcomposicao BIGINT NOT NULL,
+    idrota BIGINT NOT NULL,
+    dataHora_inicio TIMESTAMP NOT NULL,
+    dataHora_termino TIMESTAMP NULL,
+    CONSTRAINT pk_corrida PRIMARY KEY (id),
+    CONSTRAINT fk_composicao FOREIGN KEY (idcomposicao)
+        REFERENCES composicao (id),
+    CONSTRAINT fk_rota FOREIGN KEY (idrota)
+        REFERENCES rota (id)
+    -- FAZER RESTRIÇAO DE NÃO PODER 2 COMPOSICOES ESTAREM EM 2 CORRIDAS AO MESMO TEMPO
+)
+
+CREATE TABLE paradas
+(
+    idcorrida BIGINT NOT NULL,
+    idestacao BIGINT NOT NULL,
+    dataHora_parada TIMESTAMP NOT NULL,
+    CONSTRAINT pk_parada PRIMARY KEY (idcorrida, idestacao, dataHora_parada),
+    CONSTRAINT fk_corrida FOREIGN KEY (idcorrida)
+        REFERENCES corrida (id),
+    CONSTRAINT fk_estacao FOREIGN KEY (idestacao)
+        REFERENCES estacao (id)
+)
