3cd91bb817a72546b07fc4e13bcdeeed347ed6dd
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2018-11-21 22:55:48 -0200

    atualizando controllers

---
+++ linha.js
@@ -0,0 +1,28 @@
+const linhaService = require('./../models/linha');
+const bodyParser = require('body-parser')
+
+const jsonParser = bodyParser.json();
+
+module.exports = {
+    app: {},
+
+    setApp(app) { 
+        this.app = app; 
+        this.loadAPI();
+    },
+
+    loadAPI() {
+        this.app.get('/linhas', async (req, res) => {
+            res.send(JSON.stringify(await linhaService.getLinhas()));
+        });
+        
+        this.app.get('/linhas/:id', async (req, res) => {
+            res.send(JSON.stringify(await linhaService.getLinha(req.params.id)));
+        });
+
+        this.app.get('/linhas/estacao/:id', async (req, res) => {
+            res.send(JSON.stringify(await linhaService.getLinhasPorEstacao(req.params.id)));
+        });
+    }
+
+};
